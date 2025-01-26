9c3b02e898ce313b07e5ca6d6eede3906f10de77
Maisa Oliveira <maisaolisilva@gmail.com>
2025-01-26 15:50:59 -0300

    instalação do babel plugin do styled-components

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,8 +4,6 @@ import Image from "next/image";
 import Link from "next/link";
 import styled from "styled-components";
 
-
-
 const StyledDiv = styled.div`
     display: flex;
     flex-direction: column;
